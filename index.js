a748d054b685b1efe2770b1c5d9582250cc07d31
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-19 07:03:45 -0400

    only extend plain objects

--- index.js
+++ index.js
@@ -7,34 +7,32 @@
 
 'use strict';
 
+var each = require('array-each');
 var slice = require('array-slice');
 var forOwn = require('for-own');
+var isObject = require('isobject');
+
+/**
+ * Extend `object` with properties of other `objects`
+ *
+ * @name .defaults
+ * @param  {Object} `object` The target object. Pass an empty object to shallow clone.
+ * @param  {Object} `objects`
+ * @return {Object}
+ * @api public
+ */
 
 module.exports = function defaults(o, objects) {
-  if (o == null) {
-    return {};
-  }
+  if (o == null) return {};
 
-  forEach(slice(arguments, 1), function (obj) {
-    forOwn(obj, function (val, key) {
-      if (o[key] == null) {
-        o[key] = val;
-      }
-    });
+  each(slice(arguments, 1), function (obj) {
+    if (isObject(obj)) {
+      forOwn(obj, function (val, key) {
+        if (o[key] == null) {
+          o[key] = val;
+        }
+      });
+    }
   });
-
   return o;
 };
-
-function forEach(arr, cb) {
-  if (arr == null) {
-    return;
-  }
-  var len = arr.length;
-  var i = -1;
-  for (var i = 0; i < len; i++) {
-    if (cb(arr[i], i, arr) === false) {
-      break;
-    }
-  }
-}
--- test.js
+++ test.js
@@ -7,10 +7,34 @@
 
 'use strict';
 
+/* deps: mocha */
 require('should');
 var defaults = require('./');
 
 describe('defaults', function () {
+  var ctx = {};
+  beforeEach(function () {
+    ctx.foo = {aaa: 'bbb', ccc: 'ddd'};
+    ctx.bar = {ccc: 'eee', fff: 'ggg'};
+  });
+
+  it('should return an empty string when undefined.', function() {
+    defaults().should.eql({});
+  });
+
+  it('should extend the first object with missing properties from the second.', function() {
+    defaults(ctx.foo, ctx.bar).should.eql({aaa:'bbb',ccc:'ddd',fff:'ggg'});
+  });
+
+  it('should ignore non-objects.', function() {
+    defaults(ctx.foo, ctx.bar, 'baz').should.eql({aaa:'bbb',ccc:'ddd',fff:'ggg'});
+  });
+
+  it('should use the default object as context.', function() {
+    ctx.bar = {aaa: 'ddd'};
+    defaults(ctx.foo, ctx.bar).should.eql({aaa:'bbb',ccc:'ddd'}, 'should not overwrite `aaa`');
+  });
+
   it('should copy only missing properties defaults', function () {
     defaults({a: 'c'}, {a: 'bbb', d: 'c'}).should.eql({a: 'c', d: 'c'});
   });
